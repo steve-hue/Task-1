cbf34b4960771b9fe2e5f98ae6ea5804048e1e62
Steve Stagy <stevesk2001@gmail.com>
2021-11-10 23:12:07 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react"
+
+import axios from "axios"
 
 function App() {
+  const URL="https://jsonplaceholder.typicode.com/todos"
+ 
+  const [task,setTask]=useState([])
+
+  function readAPI(){
+    axios.get(URL).then((response)=>{
+      console.log(response.data)
+      
+      setTask(response.data);
+    })
+  }
+  function createAPI(){
+    axios.post(URL,{
+      title:"API has been created"
+    }).then((response)=>{
+      console.log(response.data)
+      setTask([...task,response.data])
+    })
+
+  }
+
+  function updateAPI(id){
+    axios.put(`${URL}/${id}`,{
+      title:"The API has been updated"
+    }).then((response)=>{
+      setTask(task.map((item)=>{
+        
+        if(item.id===id){
+          item=response.data        
+        }
+        return item
+      }))
+      
+    })
+  }
+
+  function delAPI(id){
+    axios.delete(`${URL}/${id}`).then((response)=>{
+     
+      setTask(task.filter((item)=>item.id!==id)) 
+    })
+  }
+
+  useEffect(()=>{
+    readAPI()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>To-Do List</h1>
+      <button onClick={createAPI}>Create</button>
+      <div>
+        {task.map((item)=>{return(
+          <div>
+          <p>{item.title}</p>
+          <button onClick={()=>updateAPI(item.id)}>update</button>
+          <button onClick={()=>delAPI(item.id)}>delete</button>
+          </div>
+        )})}
+      </div>
     </div>
   );
 }
